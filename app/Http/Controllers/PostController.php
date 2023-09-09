76d129375bad03f2a3ffc221673e454b766602cf
Draganz <112307731+DraganAbrisamWidijanto@users.noreply.github.com>
2023-09-09 15:02:24 +0700

    Second Commit Controller dan Migration

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function boomsport()
+    public function boomesport()
     {
         return view('boom');
     }
